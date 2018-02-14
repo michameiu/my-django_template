63e817e4708722cc1cf75827dbe26d8756534139
Micha Mwangi <michameiu@gmail.com>
2018-02-14 22:49:36 +0300

    changes

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/base.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import sys
 reload(sys)
-sys.setdefaultencoding("ISO-utf8")
+sys.setdefaultencoding("ISO-8859-1")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
